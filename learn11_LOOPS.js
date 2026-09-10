faeba49efd9f78a09ab2973b6c6b3e9cbb947329
kritarth-techz <kritarth20techz@gmail.com>
2026-09-11 02:15:19 +0530

    Add files via upload

---
+++ learn11_LOOPS.js
@@ -0,0 +1,31 @@
+// for loop
+let a = 1;
+
+for (let i = 0; i < 10; i++){
+    console.log(a+i);
+}
+
+let obj = {
+    name: "kritarth",
+    role: "coder"
+}
+
+for(const key in obj){
+console.log(key)
+}
+
+// while loop
+let i = 0;
+while(i<6){
+    console.log(i)
+    i++
+}
+
+// do while loop
+
+let j = 0;
+do{
+    console.log(j)
+    j++;
+
+}while(j<7)
---
+++ learn12_Function.js
@@ -0,0 +1,18 @@
+function nice(name) {
+    console.log("hey "+name+" you are nice")
+    console.log("hey "+name+" you are good")
+    console.log("hey "+name+" you are best")
+    
+}
+nice("kritarth")
+console.log("")
+nice("kirito")
+console.log("")
+function sum(a,b) {
+    // console.log(a+b)
+    return a+b
+}
+
+result = sum(2,3)
+
+console.log("the sum is:", result);
