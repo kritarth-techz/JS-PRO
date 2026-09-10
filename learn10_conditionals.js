ec4c4b1a525c2275e685b3a70e3b1ef41604231d
kritarth-techz <kritarth20techz@gmail.com>
2026-09-11 01:35:36 +0530

    Add files via upload

---
+++ learn10_conditionals.js
@@ -0,0 +1,62 @@
+let age1 = 16;
+let grace = 2;
+
+if((age1+grace)>18){
+    console.log("you are drive");
+}
+
+else{
+    console.log("you cannot drive  ");
+}
+
+
+// let grace = 2;
+
+// age += grace
+// console.log(age)
+// console.log(age + grace)
+// console.log(age - grace)
+// console.log(age * grace)
+// console.log(age / grace)
+// console.log(age ** grace)
+// console.log(age % grace)
+
+/*
+I am a 
+multiline 
+comment
+*/
+
+let age = 18;
+
+if (age == 18) {
+    console.log("You can drive");
+}
+
+else if (age == 0) {
+    console.log("Are you kidding?")
+}
+
+else if (age == 1) {
+    console.log("Are you again kidding?")
+}
+
+else {
+    console.log("You cannot drive");
+}
+
+
+a = 6;
+b = 8;
+let c = a > b ? (a - b) : (b - a);
+
+/*
+translates to:
+if(a>b){
+    let c = a - b;
+}
+else {
+    let c = a - b;
+}
+
+*/
